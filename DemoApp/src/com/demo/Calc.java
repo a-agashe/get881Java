ac534c35b6ab6b82ae9905bb1c416fa609e40a18
a-agashe <ag4b.uk@gmail.com>
2020-12-17 17:08:39 +0530

    commit2

--- DemoApp/src/com/demo/Calc.java
+++ DemoApp/src/com/demo/Calc.java
@@ -1,6 +1,9 @@
 package com.demo;
 
 public class Calc {
+	public static void method() {
+		System.out.println("Hello there, Atharva");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
