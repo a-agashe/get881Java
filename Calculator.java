98075ccd3854aa6dd014a4e909f5484e0aebd1bc
a-agashe <75656825+a-agashe@users.noreply.github.com>
2020-12-17 13:46:14 +0530

    changed

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,9 @@ class Calculator{
 public static int add(int a, int b){
   return a+b;
   }
-
+public static int subtract(int a, int b){
+  return a-b;
+  }
 public static void main(String args[]){
 
   System.out.println(add(10,5));
