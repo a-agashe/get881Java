c24ca6f37a84a3999ada1d3dc7bd590f31b1cb28
Asus <Asus@DESKTOP-4MENGKS>
2020-12-17 17:22:29 +0530

    new branch

--- DemoApp/src/com/demo/Calc.java
+++ DemoApp/src/com/demo/Calc.java
@@ -4,7 +4,10 @@ public class Calc {
 	public static void method() {
 		System.out.println("Hello there, Atharva");
 	}
-
+	public static int add(int a, int b) {
+		System.out.println(a+b);
+		return(a+b);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
