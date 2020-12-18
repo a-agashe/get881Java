4cb32e1f502c5ae6a1d7de64abb4028242b733a6
Asus <Asus@DESKTOP-4MENGKS>
2020-12-18 13:13:47 +0530

    divide method added after pulling

--- DemoApp/src/com/demo/Calc.java
+++ DemoApp/src/com/demo/Calc.java
@@ -12,6 +12,10 @@ public class Calc {
 		System.out.println(a*b);
 		return(a*b);
 	}
+	public static int divide(int a, int b) {
+		System.out.println(a*b);
+		return(a/b);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
